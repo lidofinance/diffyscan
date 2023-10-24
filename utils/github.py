64acad1473e8fd91690b9a5f423e68805d7de8d5
Roman Kolpakov <rkolpakov94@gmail.com>
2023-10-24 19:42:51 +0400

    feat: add recursive dependency search for imported contracts

--- utils/github.py
+++ utils/github.py
@@ -4,25 +4,64 @@ from utils.logger import logger
 
 def get_file_from_github(github_api_token, dependency_repo, path_to_file, dep_name):
     path_to_file = path_to_file_without_dependency(path_to_file, dep_name)
-
     user_slash_repo = parse_repo_link(dependency_repo['url'])
+    
+    direct_file_content = _get_direct_file(github_api_token, user_slash_repo, dependency_repo['relative_root'], path_to_file, dependency_repo['commit'])
+    if direct_file_content:
+        return direct_file_content
+    
+    return _recursive_search(github_api_token, user_slash_repo, dependency_repo['relative_root'], path_to_file, dependency_repo['commit'])
 
-    github_api_url = (
-        f"https://api.github.com/repos/{user_slash_repo}/contents/{dependency_repo['relative_root']}/{path_to_file}"
-    )
 
-    github_api_url += "?ref=" + dependency_repo['commit']
+def _get_direct_file(github_api_token, user_slash_repo, relative_root, path_to_file, commit):
+    github_api_url = f"https://api.github.com/repos/{user_slash_repo}/contents/{relative_root}/{path_to_file}?ref={commit}"
+    response = fetch(github_api_url, headers={"Authorization": f"token {github_api_token}"})
 
-    github_data = fetch(
-        github_api_url, headers={"Authorization": f"token {github_api_token}"}
-    )
+    if response is None:
+        return None
+    github_data = response
 
-    file_content = github_data.get("content")
+    if isinstance(github_data, dict) and github_data.get('type') == 'file':
+        file_content = github_data.get("content")
+        if not file_content:
+            logger.error(f"No file content in {path_to_file}")
+            return None
+        return base64.b64decode(file_content).decode()
 
-    if not file_content:
-        logger.error("No file content")
+    return None
 
-    return base64.b64decode(file_content).decode()
+
+def _recursive_search(github_api_token, user_slash_repo, relative_path, filename, commit, checked_dirs=None):
+    if checked_dirs is None:
+        checked_dirs = []
+
+    github_api_url = f"https://api.github.com/repos/{user_slash_repo}/contents/{relative_path}/{filename}?ref={commit}"
+    github_data = fetch(github_api_url, headers={"Authorization": f"token {github_api_token}"})
+
+    if github_data and isinstance(github_data, dict) and "content" in github_data:
+        return base64.b64decode(github_data["content"]).decode()
+
+    github_api_url = f"https://api.github.com/repos/{user_slash_repo}/contents/{relative_path}?ref={commit}"
+    github_data = fetch(github_api_url, headers={"Authorization": f"token {github_api_token}"})
+
+    if github_data and isinstance(github_data, list):
+        directories = [item['path'] for item in github_data if item['type'] == 'dir']
+
+        for dir_path in directories:
+            if dir_path in checked_dirs:
+                continue
+
+            checked_dirs.append(dir_path)
+            found_content = _recursive_search(github_api_token, user_slash_repo, dir_path, filename, commit, checked_dirs)
+            if found_content:
+                return found_content
+
+        if relative_path and relative_path not in checked_dirs:
+            checked_dirs.append(relative_path)
+            return _recursive_search(github_api_token, user_slash_repo, '', filename, commit, checked_dirs)
+
+    return None
+
 
 def path_to_file_without_dependency(path_to_file, dep_name):
     # exclude dependency prefix from path to file
