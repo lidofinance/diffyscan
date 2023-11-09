f492b35399e2745224c356d8149496c21631693b
Artyom Veremeenko <arwer13@gmail.com>
2023-11-09 21:53:39 +0300

    feat: add mantle support with goerli example config

--- utils/explorer.py
+++ utils/explorer.py
@@ -46,7 +46,32 @@ def _get_contract_from_zksync(token, zksync_explorer_hostname, contract):
 
     return (contract_name, source_files)
 
+def _get_contract_from_mantle(token, etherscan_hostname, contract):
+    etherscan_link = f"https://{etherscan_hostname}/api?module=contract&action=getsourcecode&address={contract}"
+
+    response = fetch(etherscan_link)
+
+    if response["message"] == "NOTOK":
+        logger.error("Failed", response["result"])
+        logger.error("Status", response.status_code)
+        logger.error("Response", response.text)
+        sys.exit(1)
+
+    data = response["result"][0]
+    if not data["ContractName"]:
+        logger.error("Not a contract or source code is not verified", contract)
+        sys.exit(1)
+
+    contract_name = data["ContractName"]
+    source_files = [(data["FileName"], { "content": data["SourceCode"] })]
+    for entry in data["AdditionalSources"]:
+        source_files.append((entry["Filename"], { "content": entry["SourceCode"] }))
+
+    return (contract_name, source_files)
+
 def get_contract_from_explorer(token, explorer_hostname, contract):
     if explorer_hostname.startswith("zksync"):
         return _get_contract_from_zksync(token, explorer_hostname, contract)
+    if "mantle.xyz" in explorer_hostname:
+        return _get_contract_from_mantle(token, explorer_hostname, contract)
     return _get_contract_from_etherscan(token, explorer_hostname, contract)
