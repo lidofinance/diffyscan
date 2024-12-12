3549e4a52500ed663dcaf08d2d8faf9eb2692395
Anton Kovalchuk <koval400k@gmail.com>
2024-12-12 23:03:54 +0100

    fix: use number in chain id in config

--- hardhat_configs/automaton_hardhat_config.js
+++ hardhat_configs/automaton_hardhat_config.js
@@ -2,7 +2,7 @@ module.exports = {
   solidity: "0.8.9",
   networks: {
     hardhat: {
-      chainId: process.env.CHAIN_ID,
+      chainId: Number(process.env.CHAIN_ID),
       blockGasLimit: 92000000,
       hardfork: "cancun",
     }
