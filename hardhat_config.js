7a7be3d683b36d8ab30dbc4aa2080221f2067b55
Artem G <175325367+sandstone-ag@users.noreply.github.com>
2025-04-02 16:50:39 +0400

    Holesky testnet deploy config

---
+++ hardhat_config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    solidity: "0.8.26",
+    networks: {
+        hardhat: {
+            chainId: 17000,
+            blockGasLimit: 30000000,
+            hardfork: "cancun",
+        },
+    },
+};
+
