8b650076a21be47baa3b09db367890682f5ff7a6
chasingrainbows <chasing.rainbows@lido.fi>
2025-06-18 13:55:52 +0300

    feat(orc-419): fix gitignore

--- hardhat_configs/holesky_hardhat_config.js
+++ hardhat_configs/holesky_hardhat_config.js
@@ -2,7 +2,6 @@ module.exports = {
   solidity: "0.8.9",
   networks: {
     hardhat: {
-      allowUnlimitedContractSize: true,
       chainId: 17000,
       blockGasLimit: 92000000,
       hardfork: "cancun",
